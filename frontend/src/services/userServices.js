1807dcad6d6e333c050d7956719d32e28fb83beb
danny8823 <dannyyoo714@gmail.com>
2024-09-07 08:25:01 -0700

    Added changePassword and update profile api logic

--- frontend/src/services/userServices.js
+++ frontend/src/services/userServices.js
@@ -1,13 +1,50 @@
 import axios from 'axios'
 import { BASE_URL } from '../utils/url'
+import { getUserFromStorage } from '../utils/getUserFromStorage'
 
+const token = getUserFromStorage()
+console.log('THIS IS TOKEN', token)
 export const loginAPI = async({email,password}) => {
     console.log(email,password)
     const response = await axios.post(`${BASE_URL}/users/login`,{
         email,
         password
     })
-    console.log(response)
+
     return response.data
 }
 
+export const registerAPI = async({email,password,username}) => {
+    const response = await axios.post(`${BASE_URL}/users/register`, {
+        email,
+        password,
+        username
+    })
+
+    return response.data
+}
+
+export const changePasswordAPI = async(newPassword) => {
+    const response = await axios.put(`${BASE_URL}/users/change-password`, {
+        newPassword
+    },{
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    return response.data
+}
+
+export const updateProfileAPI = async({email,username}) => {
+    const response = await axios.put(`${BASE_URL}/users/change-profile`, {
+        email, username
+    },
+    {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    return response.data
+} 
