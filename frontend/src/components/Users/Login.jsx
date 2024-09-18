80023cacda438a6c0687035d482884f50acc7054
danny8823 <dannyyoo714@gmail.com>
2024-09-18 06:29:47 -0700

    Finished

--- frontend/src/components/Users/Login.jsx
+++ frontend/src/components/Users/Login.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
 import { useFormik } from 'formik';
 import {useMutation} from '@tanstack/react-query'
 import { loginAPI } from '../../services/userServices';
 import {useDispatch} from 'react-redux'
 import { loginAction } from '../../redux/slice/authSlice';
+import './Login.css'
 
 const Login = () => {
   const dispatch = useDispatch()
-
+  const navigate = useNavigate()
   const {mutateAsync, isPending, isError, error, isSuccess} = useMutation({
     mutationFn: loginAPI,
     mutationKey: ['login']
@@ -23,6 +25,7 @@ const Login = () => {
         .then((data)=>{
           dispatch(loginAction(data))
           localStorage.setItem('userInfo',JSON.stringify(data))
+          navigate('/shop')
         })
         .catch((error)=>{
           console.log(error)
@@ -31,27 +34,30 @@ const Login = () => {
   })
 
   return (
-    <form onSubmit = {formik.handleSubmit}>
-      {isPending && <h1>One second!</h1>}
-      {isSuccess && <h1>Login successful.</h1>}
-      {isError && <h1>{error.response.data.message}</h1>}
-      <label htmlFor = "email">Email Address</label>
-      <input 
-        id = "email"
-        name = "email"
-        type = "email"
-        onChange = {formik.handleChange}
-        value = {formik.values.email}
-      />
-      <input 
-        id = "password"
-        name = "password"
-        type = "password"
-        onChange = {formik.handleChange}
-        value = {formik.values.password}
-      />
-      <button type = 'submit'>Submit</button>            
-    </form>
+    <div className = 'form-container'>
+       <form onSubmit = {formik.handleSubmit}>
+        {isPending && <h1>One second!</h1>}
+        {isSuccess && <h1>Login successful.</h1>}
+        {isError && <h1>{error.response.data.message}</h1>}
+        <label htmlFor = "email">Email Address</label>
+        <input 
+          id = "email"
+          name = "email"
+          type = "email"
+          onChange = {formik.handleChange}
+          value = {formik.values.email}
+        />
+        <input 
+          id = "password"
+          name = "password"
+          type = "password"
+          onChange = {formik.handleChange}
+          value = {formik.values.password}
+        />
+        <button type = 'submit'>Submit</button>            
+      </form>
+    </div>
+   
   )
 }
 
